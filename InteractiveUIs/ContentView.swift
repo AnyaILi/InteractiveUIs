63952b06b3c68530cab3fe6ae28ccb33fd7b773c
AnyaILi <anyali0219@gmail.com>
2023-07-13 13:04:05 -0700

    Added interactive functions

--- InteractiveUIs/ContentView.swift
+++ InteractiveUIs/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var TextTitle = "What is your name?"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(TextTitle)
+                .font(.title)
+            TextField("Type name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title2)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                TextTitle = "Welcome \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
